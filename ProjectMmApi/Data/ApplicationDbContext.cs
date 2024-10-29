f4952a2bd0b4e8671c25eb9b49e04adea163a2b9
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-10-29 22:17:42 +0545

    Add project files.

---
+++ ProjectMmApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectMmApi.Models.Entities;
+
+namespace ProjectMmApi.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ProjectMmApi/Models/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectMmApi.Models.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public required string FullName { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
