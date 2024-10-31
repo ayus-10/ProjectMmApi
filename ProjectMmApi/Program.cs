93a423470fc212dbd3682fb764838a1ce5bc8e76
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-10-31 20:57:25 +0545

    Configure package JwtBearer

--- ProjectMmApi/Program.cs
+++ ProjectMmApi/Program.cs
@@ -2,20 +2,43 @@ using Microsoft.EntityFrameworkCore;
 using ProjectMmApi.Data;
 using ProjectMmApi.Services;
 using ProjectMmApi.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+var jwtConfig = builder.Configuration.GetSection("JwtConfig");
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseMySQL(builder.Configuration.GetConnectionString("Default") ?? String.Empty));
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidIssuer = jwtConfig["Issuer"],
+        ValidAudience = jwtConfig["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtConfig["Key"] ?? String.Empty)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+builder.Services.AddAuthorization();
+
 builder.Services.AddScoped<IPasswordHasher, PasswordHasher>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,6 +46,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
---
+++ ProjectMmApi/Utilities/Validator.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Mail;
+
+namespace ProjectMmApi.Utilities
+{
+    public static class Validator
+    {
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
+                var emailAddress = new MailAddress(email);
+                return emailAddress != null;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool IsStrongPassword(string password)
+        {
+            bool hasMinimumLength = password.Length >= 8;
+            bool hasUppercase = password.Any(c => Char.IsUpper(c));
+            bool hasLowercase = password.Any(c => Char.IsLower(c));
+            bool hasWhiteSpace = password.Any(c => Char.IsWhiteSpace(c));
+
+            return hasMinimumLength && hasUppercase && hasLowercase && (!hasWhiteSpace);
+        }
+    }
+}
